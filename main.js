872d4546d5213da50e3a77fe73221b352aeb559d
rmorales300 <rmorales300@student.alamo.edu>
2025-01-30 20:50:20 -0600

    new index

---
+++ main.js
@@ -0,0 +1,23 @@
+var health = 100;
+console.log(health); // Global variable
+
+{
+    let drain = 50;
+    console.log(drain); //block variable
+}
+
+let x = 500;
+let y = 250;
+let z = x + y;
+console.log(z);
+
+let greeting = "Hello if im not following the assignment correctly please let me know so I can set up a zoom metting to correct the problem";
+console.log(greeting);// string variable
+
+
+const player = {
+    name: "Robert Morales",
+    age: 23,
+    country: "USA" 
+};
+console.log(player);
